c6971e5511f2f13583472d9c9e38fa6b023d0bd6
Stephen Hila <stephen.hila@gmail.com>
2022-01-19 21:57:18 +0800

    Implement map displaying current position - windows+android

--- Flightbud.Xamarin.Forms/Flightbud.Xamarin.Forms/MapPage.xaml.cs
+++ Flightbud.Xamarin.Forms/Flightbud.Xamarin.Forms/MapPage.xaml.cs
@@ -1,20 +1,33 @@
-﻿using System;
+﻿using Flightbud.Xamarin.Forms.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
+using Map = Xamarin.Forms.Maps.Map;
 
 namespace Flightbud.Xamarin.Forms
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+        MapPageViewModel viewModel = new MapPageViewModel(new Map { IsShowingUser = true, MapType = MapType.Hybrid }, null);
         public MapPage()
         {
             InitializeComponent();
+            BindingContext = viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            viewModel.CurrentLocation = await Geolocation.GetLocationAsync(new GeolocationRequest(GeolocationAccuracy.High, TimeSpan.FromSeconds(60)));
+            viewModel.Update();
         }
     }
 }
---
+++ Flightbud.Xamarin.Forms/Flightbud.Xamarin.Forms/ViewModels/MapPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms.Maps;
+using Map = Xamarin.Forms.Maps.Map;
+
+namespace Flightbud.Xamarin.Forms.ViewModels
+{
+    public class MapPageViewModel : ViewModelBase
+    {
+        public Map Map { get; set; }
+        public MapSpan MapSpan { get; set; }
+        public Position MapCenter { get; set; }
+
+        Location _currentLocation;
+        public Location CurrentLocation
+        { 
+            get
+            {
+                return _currentLocation;
+            }
+            set
+            {
+                _currentLocation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MapPageViewModel(Map map, Location location)
+        {
+            Map = map;
+            CurrentLocation = location;
+        }
+
+        public void Update()
+        {
+            MapCenter = new Position(_currentLocation.Latitude, _currentLocation.Longitude);
+            MapSpan = new MapSpan(MapCenter, 0.01, 0.01);
+            Map.MoveToRegion(MapSpan);
+        }
+    }
+}
