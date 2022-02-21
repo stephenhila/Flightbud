135865878bc34d3b19e7569616ac9ba28f1032db
Stephen Hila <stephen.hila@gmail.com>
2022-02-21 12:48:27 +0800

    Fix for Defect #14, by caching current location, and relying on SimConnect to do variable updates in the background with SimConnectUpdateFrequency.SIM_Frame.

--- Flightbud.SimConnect.Api/Controllers/SimVarController.cs
+++ Flightbud.SimConnect.Api/Controllers/SimVarController.cs
@@ -16,6 +16,7 @@ namespace Flightbud.SimConnect.Api.Controllers
 
         private readonly ILogger<SimVarController> _logger;
         private readonly Dictionary<string, SimVarDefinition> simVarVariables = SimVarUnits.DefaultUnits;
+        static Location location;
 
         public SimVarController(ILogger<SimVarController> logger)
         {
@@ -23,94 +24,132 @@ namespace Flightbud.SimConnect.Api.Controllers
             SimConnectHelper.SimConnectHelper.SimData += SimConnect_DataReceived;
         }
 
-        SimConnectVariableValue result;
+        //[HttpGet(Name = "GetGpsPosition")]
+        //public Location GetGpsPosition()
+        //{
+        //    Location location = default;
+
+        //    lock (_requestLock)
+        //    {
+        //        if (!SimConnectHelper.SimConnectHelper.IsConnected)
+        //        {
+        //            try
+        //            {
+        //                SimConnectHelper.SimConnectHelper.Connect();
+
+        //                // get altitude
+        //                UpdateSimVarResult("GPS POSITION ALT");
+        //                location.Altitude = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
+
+        //                // get latitude
+        //                UpdateSimVarResult("GPS POSITION LAT");
+        //                location.Latitude = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
+
+        //                // get longitude
+        //                UpdateSimVarResult("GPS POSITION LON");
+        //                location.Longitude = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
+
+        //                UpdateSimVarResult("GPS GROUND TRUE HEADING");
+        //                location.Course = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
+
+        //                UpdateSimVarResult("GPS GROUND SPEED");
+        //                location.Speed = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
+
+        //            }
+        //            catch (InvalidOperationException iox)
+        //            {
+        //                _logger.LogError(iox, iox.Message);
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                _logger.LogError(ex, ex.Message);
+        //            }
+        //            finally
+        //            {
+        //                SimConnectHelper.SimConnectHelper.Disconnect();
+        //            }
+        //        }
+        //    }
+
+        //    Thread.Sleep(500);
+
+        //    return location;
+        //}
 
         [HttpGet(Name = "GetGpsPosition")]
         public Location GetGpsPosition()
         {
-            Location location = default;
-
             lock (_requestLock)
             {
-                if (!SimConnectHelper.SimConnectHelper.IsConnected)
+                try
                 {
-                    try
+                    if (!SimConnectHelper.SimConnectHelper.IsConnected)
                     {
+                        location = default;
+
                         SimConnectHelper.SimConnectHelper.Connect();
 
-                        // get altitude
-                        UpdateSimVarResult("GPS POSITION ALT");
-                        location.Altitude = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
+                        InvokeUpdateSimVar("GPS POSITION ALT");
+                        InvokeUpdateSimVar("GPS POSITION LAT");
+                        InvokeUpdateSimVar("GPS POSITION LON");
+                        InvokeUpdateSimVar("GPS GROUND TRUE HEADING");
+                        InvokeUpdateSimVar("GPS GROUND SPEED");
 
-                        // get latitude
-                        UpdateSimVarResult("GPS POSITION LAT");
-                        location.Latitude = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
+                        const int resultDelayCheckMilliseconds = 100;
+                        const int maxWaitForResultMilliseconds = Constants.LOCATION_TIMEOUT;
 
-                        // get longitude
-                        UpdateSimVarResult("GPS POSITION LON");
-                        location.Longitude = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
-
-                        UpdateSimVarResult("GPS GROUND TRUE HEADING");
-                        location.Course = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
-
-                        UpdateSimVarResult("GPS GROUND SPEED");
-                        location.Speed = Convert.ToDouble((result.Value as object[]).FirstOrDefault());
-
-                    }
-                    catch (InvalidOperationException iox)
-                    {
-                        _logger.LogError(iox, iox.Message);
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, ex.Message);
-                    }
-                    finally
-                    {
-                        SimConnectHelper.SimConnectHelper.Disconnect();
+                        var endWaitTime = DateTime.Now.AddMilliseconds(maxWaitForResultMilliseconds);
+                        while (location.Latitude == default && location.Longitude == default && endWaitTime > DateTime.Now)
+                        {
+                            Thread.Sleep(resultDelayCheckMilliseconds); // Wait to receive the value
+                        }
                     }
                 }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, ex.Message);
+                    SimConnectHelper.SimConnectHelper.Disconnect();
+                }
             }
-
-            Thread.Sleep(500);
-
             return location;
         }
 
-        private void UpdateSimVarResult(string simVarFieldName)
+        private void InvokeUpdateSimVar(string simVarFieldName)
         {
-            result = null;
-
             List<SimConnectVariable> failures = new List<SimConnectVariable>();
 
             var simVarAltitude = simVarVariables.FirstOrDefault(v => v.Key == simVarFieldName);
             SimConnectVariable request = new SimConnectVariable { Name = simVarAltitude.Value.Name, Unit = simVarAltitude.Value.DefaultUnit };
-            int requestId = SimConnectHelper.SimConnectHelper.GetSimVar(request, (int)SimConnectUpdateFrequency.Once);
+            int requestId = SimConnectHelper.SimConnectHelper.GetSimVar(request, (int)SimConnectUpdateFrequency.SIM_Frame);
 
             if (requestId == -1)
             {
                 throw new InvalidOperationException($"cannot get SimVar field {simVarFieldName}");
             }
 
-            const int resultDelayCheckMilliseconds = 100;
-            const int maxWaitForResultMilliseconds = Constants.LOCATION_TIMEOUT;
-
             SimConnectHelper.SimConnectHelper.GetSimVar(requestId);
-            var endWaitTime = DateTime.Now.AddMilliseconds(maxWaitForResultMilliseconds);
-
-            while (result == null && endWaitTime > DateTime.Now)
-            {
-                Thread.Sleep(resultDelayCheckMilliseconds); // Wait to receive the value
-            }
-            if (result == null)
-            {
-                failures.Add(request);
-            }
         }
 
         private void SimConnect_DataReceived(object sender, SimConnectVariableValue e)
         {
-            result = e;
+            switch (e.Request.Name)
+            {
+                case "GPS POSITION ALT":
+                    location.Altitude = Convert.ToDouble((e.Value as object[]).FirstOrDefault());
+                    break;
+                case "GPS POSITION LAT":
+                    location.Latitude = Convert.ToDouble((e.Value as object[]).FirstOrDefault());
+                    break;
+                case "GPS POSITION LON":
+                    location.Longitude = Convert.ToDouble((e.Value as object[]).FirstOrDefault());
+                    break;
+                case "GPS GROUND TRUE HEADING":
+                    location.Course = Convert.ToDouble((e.Value as object[]).FirstOrDefault());
+                    break;
+                case "GPS GROUND SPEED":
+                    location.Speed = Convert.ToDouble((e.Value as object[]).FirstOrDefault());
+                    break;
+            }
         }
     }
 }
